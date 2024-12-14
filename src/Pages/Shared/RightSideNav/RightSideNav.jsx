5135f2c3f05049f5ffecb77e9017793df0878327
Mazharul Islam <mazharul3islam0@gmail.com>
2024-12-14 12:59:30 +0600

    added google button

--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,8 +1,19 @@
+import { FaGoogle } from "react-icons/fa";
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className="text-2xl">Right Side</h2>
+            <div className="p-4 space-y-3">
+                <h2 className="text-3xl font-semibold">Login With</h2>
+                <button className="btn btn-outline w-full">
+                <FaGoogle />
+                    Google
+                </button>
+                <button className="btn btn-outline w-full">
+                <FaGoogle />
+                    Google
+                </button>
+            </div>
         </div>
     );
 };
