eb744e03c9fa01250934af3ec16dba297fc1fed4
Mazharul Islam <mazharul3islam0@gmail.com>
2024-12-15 11:58:50 +0600

    push

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
         <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/about'>About</NavLink></li>
         <li><NavLink to='/career'>Career</NavLink></li>
+        <li><NavLink to='/login'>Login</NavLink></li>
 
     </>
 
@@ -48,7 +49,7 @@ const Navbar = () => {
                             src={userDefaultPic} />
                     </div>
                 </div>
-                <Link>
+                <Link to='/login'>
                     <button className="btn">Login</button>
                 </Link>
             </div>
