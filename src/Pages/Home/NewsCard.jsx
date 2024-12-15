eab0a73c9ddff3fe71f7bae80fd35b7ac8fc510d
Mazharul Islam <mazharul3islam0@gmail.com>
2024-12-16 01:13:20 +0600

    push

---
+++ src/Pages/Home/NewsCard.jsx
@@ -0,0 +1,16 @@
+
+
+const NewsCard = ({news}) => {
+
+    const {title} = news;
+
+    return (
+        <div>
+            <h4 className="text-2xl">
+                {title}
+            </h4>
+        </div>
+    );
+};
+
+export default NewsCard;
