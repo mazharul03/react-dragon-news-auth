b44364c3a989b163c9049094286eb63163ca3c53
Mazharul Islam <mazharul3islam0@gmail.com>
2024-12-19 15:00:14 +0600

    Create Private route and do not redirect on reload

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
 
-const PrivateRoute = () => {
-    const {user} = useContext (AuthContext)
-    return (
-        <div>
-            
-        </div>
-    );
+const PrivateRoute = ({children}) => {
+    
+    const {user, loading} = useContext (AuthContext);
+
+    const location = useLocation();
+    console.log(location);
+
+    if(loading){
+        return <span className="loading loading-infinity loading-lg"></span>
+    }
+
+    if(user){
+        return children
+    }
+
+    return <Navigate to='/login'></Navigate>;
 };
 
 export default PrivateRoute;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import News from "../Pages/News/News";
+import PrivateRoute from "./PrivateRoute";
 
 const routes = createBrowserRouter([
     {
@@ -17,7 +18,7 @@ const routes = createBrowserRouter([
             },
             {
                 path:'/news/:id',
-                element: <News></News>
+                element: <PrivateRoute><News></News></PrivateRoute>
             },
             {
                 path: '/login',
